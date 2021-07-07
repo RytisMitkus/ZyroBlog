d1f7117ea1c89f968248cbae97ec1e0f140dd691
RytisMitkus <rytis@mitkus.lt>
2021-07-07 14:20:11 +0300

    changed db connection according to ZJB code <3

--- back/controllers/userController.js
+++ back/controllers/userController.js
@@ -1,78 +1,69 @@
-const pool = require('../mysql/db')
+const db = require('../mysql/db')
 
 const registerUser = (req, res) => {
     const user = req.body
     const { firstName, lastName, email, password, isAdmin } = user
 
-    pool.getConnection((err, connection) => {
 
-        connection.query(`INSERT INTO users (FirstName, LastName, password, email, isAdmin) VALUES ('${firstName}', '${lastName}', '${password}', '${email}', '${isAdmin}');`, (err, result) => {
-            connection.release();
 
-            if (!err) {
-                res.json({
-                    isAdmin,
-                    email,
-                    firstName,
-                    login: true
-                })
-            } else {
-                console.log(err)
-                res.json({
-                    error: true,
-                    msg: `User with email - ${email} already exists`
-                })
-            }
-            return result
-        })
+    db.query(`INSERT INTO users (FirstName, LastName, password, email, isAdmin) VALUES ('${firstName}', '${lastName}', '${password}', '${email}', '${isAdmin}');`, (err, result) => {
+
+        if (!err) {
+            res.json({
+                isAdmin,
+                email,
+                firstName,
+                login: true
+            })
+        } else {
+            console.log(err)
+            res.json({
+                error: true,
+                msg: `User with email - ${email} already exists`
+            })
+        }
+        return result
     })
+
 }
 
 const getUsers = (req, res) => {
-    pool.getConnection((err, connection) => {
-
-        connection.query(`SELECT * FROM users;`, (err, result) => {
-            connection.release();
-            if (!err) {
-                const usersList = result.map((user) => {
-                    const filteredUser = {
-                        firstName: user.firstName,
-                        lastName: user.lastName,
-                        email: user.email,
-                        isAdmin: user.isAdmin
-                    }
-                    return filteredUser
-                })
-                res.json(usersList)
-            } else {
-                res.json({
-                    error: true,
-                    msg: `Something went wrong, please contact administrator`
-                })
-            }
-            return result
-        })
+    db.query(`SELECT * FROM users;`, (err, result) => {
+        if (!err) {
+            const usersList = result.map((user) => {
+                const filteredUser = {
+                    firstName: user.firstName,
+                    lastName: user.lastName,
+                    email: user.email,
+                    isAdmin: user.isAdmin
+                }
+                return filteredUser
+            })
+            res.json(usersList)
+        } else {
+            res.json({
+                error: true,
+                msg: `Something went wrong, please contact administrator`
+            })
+        }
+        return result
     })
 }
 
 const loginUser = (req, res) => {
-    pool.getConnection((err, connection) => {
-        const { email, password } = req.body
-        console.log(email, password)
-        connection.query(`SELECT * FROM users WHERE email='${email}' AND password='${password}';`, (err, result) => {
-            connection.release();
-            if (result.length > 0) {
-                res.json(result)
-            } else {
-                res.json({
-                    error: true,
-                    msg: `E-mail of password is incorrect`
-                })
-            }
-            return result
-        })
+    const { email, password } = req.body
+    console.log(email, password)
+    db.query(`SELECT * FROM users WHERE email='${email}' AND password='${password}';`, (err, result) => {
+        if (result.length > 0) {
+            res.json(result)
+        } else {
+            res.json({
+                error: true,
+                msg: `E-mail of password is incorrect`
+            })
+        }
+        return result
     })
-
 }
 
 module.exports = { registerUser, getUsers, loginUser }
--- back/mysql/db.js
+++ back/mysql/db.js
@@ -6,7 +6,27 @@ const options = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE
 }
-const pool = mysql.createPool(options)
-pool.query = util.promisify(pool.query);
+function setupMysqlPool(options) {
+    const pool = mysql.createPool(options);
+
+    // Check connection to MySql
+    pool.getConnection((err, connection) => {
+        if (err) {
+            const errorMsg = E_ERROR_CODES[err.code] || 'DB pool error';
+            logger.log(errorMsg, err);
+            // eslint-disable-next-line no-console
+            console.error(errorMsg, err);
+        }
+        if (connection) {
+            connection.release();
+        }
+    });
+
+    pool.query = util.promisify(pool.query);
+
+    return pool;
+}
+
+const pool = setupMysqlPool(options);
 
 module.exports = pool;
